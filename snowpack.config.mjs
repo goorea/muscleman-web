d00aba7b0c1b7d955d117e29495a55804783365f
hyjeong49 <hyjeong49@coupang.com>
2022-04-17 19:46:06 +0900

    [feat] Snowpack으로 마이그레이션
    #7

---
+++ snowpack.config.mjs
@@ -0,0 +1,41 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-typescript',
+      {
+        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+      },
+    ],
+    '@snowpack/plugin-webpack',
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    bundle: true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    port: 3000,
+  },
+  buildOptions: {
+    sourcemap: true,
+  },
+  alias: {
+    /* ... */
+  },
+  testOptions: {
+    files: ["**/*.@(spec|test).*"],
+  },
+};
