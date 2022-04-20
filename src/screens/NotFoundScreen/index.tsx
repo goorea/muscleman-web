bb996c4d2c643ea92eaae1f578b3a699520c2b54
hyjeong49 <hyjeong49@coupang.com>
2022-04-21 00:04:14 +0900

    [feat] NotFound 404 화면
    #28

--- src/screens/NotFoundScreen/index.tsx
+++ src/screens/NotFoundScreen/index.tsx
@@ -1,14 +1,38 @@
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 
-type P = unknown;
-
-const NotFoundScreen: React.FC<P> = () => {
-  return (
-    <Container component="main" maxWidth="xs">
-      Not Found Screen
-    </Container>
-  );
-};
+const NotFoundScreen: React.FC = () => (
+  <Container
+    component="main"
+    disableGutters={true}
+    maxWidth={false}
+    sx={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+    }}
+  >
+    <Typography
+      variant="h1"
+      gutterBottom={false}
+      sx={{ color: 'primary.main', fontWeight: 'bold' }}
+    >
+      404
+    </Typography>
+    <Typography variant="h5">이런! 페이지를 찾을 수 없습니다.</Typography>
+    <Typography paragraph={true} align="center" mt={2}>
+      죄송합니다. 찾으시는 페이지가 없습니다.
+      <br />
+      만약 무언가가 고장났다고 생각되신다면 문의해주세요!
+    </Typography>
+    <Button variant="contained" href="mailto:goorea1026@gmail.com">
+      문의하기
+    </Button>
+  </Container>
+);
 
 export default NotFoundScreen;
